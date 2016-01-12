39ca7d87bebecd177e6593ddf1d03c09d0481f50
mies <mies@wercker.com>
2016-01-12 17:43:48 +0100

    init

---
+++ app.js
@@ -0,0 +1,57 @@
+// Requires
+
+var express = require('express');
+var mongoose = require ("mongoose");
+
+var app = express();
+app.use(express.bodyParser());
+
+var Todo = require('./models/todo');
+
+// Configure express
+app.get('development', function() {
+  mongoose.connect('mongodb://localhost/todos');
+});
+
+app.get('test', function() {
+  mongoose.connect('mongodb://'+ process.env.WERCKER_MONGODB_HOST + '/todos');
+});
+
+app.get('production', function() {
+  mongoose.connect('mongodb://localhost/todos');
+});
+
+// Routes
+app.get('/', function(req, res) {
+  res.send({'version' : '0.0.1'});
+});
+
+app.get('/todos', function(req, res) {
+  Todo.find(function(err, result) {
+    res.send(result);
+  });
+});
+
+app.get('/todos/:author', function(req, res) {
+  Todo.findOne({'author': req.params.author}, function(err, result) {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    } else {
+      res.send({result: result});
+    }
+  });
+});
+
+app.post('/todos', function(req, res) {
+  new Todo({action: req.body.action, author: req.body.author}).save();
+  res.send({'new todo' : req.body.action});
+});
+
+// startup server
+port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on port number: ", port);
+});
+
+module.exports = app;
---
+++ models/todo.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var todoSchema= new Schema({
+  action: String,
+  author: String,
+  creationDate: {type: Date, 'default': Date.now},
+});
+
+module.exports = mongoose.model('Todo', todoSchema);
